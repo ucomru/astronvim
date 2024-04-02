947ca820928dec1223d5a27eab57f691697b06f0
Timur U <ucomru@pm.me>
2024-04-02 18:00:16 +0300

    Added astrocommunity packs: lua, golang, python-ruff

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,10 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
+-- if true then return {} end
+-- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack
 
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python-ruff" },
 }
