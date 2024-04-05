295238a8d898bd8a58666aaeeb20484b0fd3d0fe
Timur U <ucomru@pm.me>
2024-04-05 10:42:48 +0300

    Added packs (rust, md, json, bash etc.)

--- lua/community.lua
+++ lua/community.lua
@@ -4,7 +4,13 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.html-css" },  
 }
